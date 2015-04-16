520f10f836cee49dc610556e5ba0ed0fd32b24cf
Fille881 <filip_lilja@hotmail.com>
2015-04-16 11:26:41 +0200

    JSONLint editing

--- js/gamescript.js
+++ js/gamescript.js
@@ -1,33 +1,25 @@
 
 
-countryArray = ["Sweden", "Denmark", "Norway", "Finland", "France", "Great Britain", "Germany", "Netherlands"];
-country = countryArray[0];
+var countryArray = ["Sweden", "Denmark", "Norway", "Finland", "France", "Great Britain", "Germany", "Netherlands"];
+var country = countryArray[0];
 
-questionsArray = ["Swedenquestion", "Denmarkqustion", "Norwayquestion"];
-answerArray = ["Sweden correct", "Denmark correct", "Norway correct"];
+var questionsArray = ["Swedenquestion", "Denmarkqustion", "Norwayquestion"];
+var answerArray = ["Sweden correct", "Denmark correct", "Norway correct"];
 
-gameLoop = true;
-while (gameLoop == true) {
-	playerChoice = prompt("You are in "+country+", choose where you want to go next");
+var gameLoop = true;
+while (gameLoop === true) {
+	var playerChoice = prompt("You are in " + country + ", choose where you want to go next");
 	//console.log(countryArray.length);
-
-
-	for (i=0; i<countryArray.length; i++) {
-		if (playerChoice == countryArray[i]) {
-			answer = prompt("Qustion:"+questionsArray[i]);
-			
-				if (answer == answerArray[i]){
-					
-					country = countryArray[i];
+	for (i = 0; i < countryArray.length; i++) {
+		if (playerChoice === countryArray[i]) {
+			var answer = prompt("Qustion:" + questionsArray[i]);
+				if (answer === answerArray[i]) {
+				country = countryArray[i];
 				}
 				else {
 					alert("Game over");
 					gameLoop = false;
 				}
-				
 		}
-		
 	}
-	
-	
 }
