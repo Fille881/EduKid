170f035914a83a4364f49d95b2d2d21a48944104
marin mikulic <shaffe@users.noreply.github.com>
2015-04-22 08:57:54 +0200

    edukid based on jvectormaps
    the game uses scalable vector graphics (svg) files

---
+++ js/map.js
@@ -0,0 +1,30 @@
+$(function(){
+  map = new jvm.Map({
+    container: $('#map'),
+    map: 'world_mill_en',//map file
+    backgroundColor: ['#1E90FF'],//map background color    
+    regionsSelectable:true,//this needs to be true to be able to select a region
+
+    onRegionClick: function(event, code){
+      var map = $('#map').vectorMap('get', 'mapObject');//gets the map data
+      var regionName = map.getRegionName(code);//gets code of current country
+      $.getJSON('../countries.json', function(data) {
+        for (var i in data.country) {//loops through json file
+          if (data.country[i].name === regionName){//finds which json field matches the current region country name
+            var answer = prompt("Question for " + code + ": " + data.country[i].question, "Not much brah.");//asks the question
+            if(data.country[i].answer === answer){//checks if answer correct
+              console.log("Correct!");//test purposes
+              map.setSelectedRegions(code);
+            }else{
+              console.log("Wrong. Try again.");
+              map.clearSelectedRegions(code);
+              console.log(code);//test purposes
+            }
+          }
+        }
+      });
+    },
+
+  });
+});
+
