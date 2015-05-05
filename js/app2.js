0d6298ebc9c509e93c7956a17aa64ba27d87a756
codewithcode <codewithcode@users.noreply.github.com>
2015-05-05 02:19:14 +0200

    Refactored the code some more.

--- js/app2.js
+++ js/app2.js
@@ -1,105 +1,120 @@
-var app = {};
+var app = {}; // Our single global variable, that holds useful things! =)
 app.settings = {
   mapname: 'world_mill_en',
 };
-app.data = {}; // This will hold the json map-data
+app.countries = {}; // This will hold the json map-data
+app.points = 0;
+app.map = {}; // Will hold the map plugin
 
 
 
-//Directs user to correct map
+// When the browser has finished loading
 $(document).ready(function() {
-  console.log("Hello I am ready!");
-    $(".btn").click(function() {
-        window.location.assign('map_game/' + this.id + '.html');
-    });
+  'use strict';
+  console.log("Good day, we are running!");
+
+  // Let us start up this application
+  loadMap();
+
 });
 
-var points = parseInt('0');
-
+// Fetches json-data for map and starts the jquery map plugin
 function loadMap() {
   'use strict';
   $.getJSON('../maps/' + app.settings.mapname + '.json', function( data ) {
-      app.data = data;
+      app.countries = data.country; // Store the json-data in our app-object, so we can use it everywhere
 
-      map = new jvm.Map({
-      container: $('#map'),
-      map: app.settings.mapname,
-      backgroundColor: ['#1E90FF'],
-      regionsSelectable: true,
-      markersSelectable: true,
-      selectedRegions: [getRandomCountryCode(data)],
-      onRegionTipShow: function(event, label, code){
-        var mapObj = $('#map').vectorMap('get', 'mapObject');
-        var regionName = mapObj.getRegionName(code);
-        var x;
-        for (var i in data.country) {
-        if(regionName === data.country[i].name){
-          x = data.country[i].points;
-        }
-      }
-       label.html('<img src=\"../img/flags/squareflags/'+ name + '.png\" width=\"16px\" height=\"11px\""><br>This country is worth ' + x + ' points.');
+      app.map = new jvm.Map({
+        container: $('#map'),
+        map: app.settings.mapname,
+        backgroundColor: ['#1E90FF'],
+        regionsSelectable: true,
+        markersSelectable: true,
+        selectedRegions: [getRandomCountryCode(data)],
+        onRegionTipShow: function(event, label, code) {
+          var mapObj = $('#map').vectorMap('get', 'mapObject');
+          var regionName = mapObj.getRegionName(code);
+          var  countrypoints;
+          console.log("Region name:", regionName);
 
-     },
-      onRegionClick: function(event, code){
-        askQuestionOnClick(app.data, code, app.settings.mapname);
-      },
-    }); // new jvm.Map
-  }); //ajax
+          data.country.forEach(function(country) {
+            if (regionName === country.name) {
+              //countrypoints = data.country[i].points;
+              countrypoints = 5; // Tmp
+            }
+          });
+
+          var labelflag = '<img src="../img/flags/'+ code + '.png" width="16px" height="11px">';
+          var labelpoints = '<br>This country is worth ' + countrypoints + ' points.';
+          label.html(labelflag + labelpoints);
+
+
+       },
+        onRegionClick: function(event, code){
+          askQuestionOnClick(code);
+        },
+
+      }); // new jvm.Map
+
+  }); //$.getJSON
 }
 
 //Returns a random country code from JSON file
-function getRandomCountryCode(data){
+function getRandomCountryCode(){
   'use strict';
-  var countries = [], rndCountry;
-  for (var i in data.country) {
-    console.log('country:', i);
-
-    countries.push(data.country[i].code);
-  }
-  rndCountry = countries[Math.floor(Math.random()*countries.length)];
-  return rndCountry;
+  var random_id = Math.floor(Math.random()*app.countries.length);
+  return app.countries[random_id].code;
 }
 
 //Ask question and validates answer
-function askQuestionOnClick(data, code){
+function askQuestionOnClick(code){
+  'use strict';
   var mapObj = $('#map').vectorMap('get', 'mapObject');
   var regionName = mapObj.getRegionName(code);
-  var counter = 0;
+  //var counter = 0;
   swalPrompt(regionName);
 }
 
 function swalPrompt(regionName){
-  $.getJSON('../maps/' + app.settings.mapname + '.json', function( data ) {
-    for (var i in data.country){
-      if(regionName === data.country[i].name){
-        currentAnswer = data.country[i].answer;
-        swal({
-          title: data.country[i].name,
-          text: data.country[i].question,
-          type: "input",
-          showCancelButton: true,
-          closeOnConfirm: false,
-          animation: "slide-from-top",
-          inputPlaceholder: "Write your answer"
-          },
-          function(inputValue){
-            if (inputValue === false) return false;
-            if (inputValue === "") {
-              swal.showInputError("You need to write something!");
-              return false
-            }
-            if (inputValue == currentAnswer){
-              console.log("correct");
-              swal("Nice!", "You wrote: " + inputValue, + "success" + data.country[i].points);
-              points = points + parseInt(data.country[i].points);
-			  $('#points').text("You've got" + " " + points + " " +"points");
+  'use strict';
+    var currentAnswer;
 
-            }else{
-              console.log("incorrect");
-              console.log(inputValue);
+    // Filter out the country with the selected region name
+    var country = app.countries.filter(function(country) {
+      return country.name == regionName;
+    })[0]; // Get the first element, it will only be one
+
+    if (country) {
+      swal({
+        title: country.name,
+        text: country.question,
+        type: "input",
+        showCancelButton: true,
+        closeOnConfirm: false,
+        animation: "slide-from-top",
+        inputPlaceholder: "Write your answer"
+        },
+        function(inputValue){ // Called when we press "Ok"
+          if (inputValue === false) return false;
+          if (inputValue === "") {
+            swal.showInputError("You need to write something!");
+            return false;
+          }
+          if (inputValue == country.answer){
+            console.log("Correct answer.");
+            swal("Nice!", "You wrote: " + inputValue, + "success" + app.countries[i].points);
+            if(country.points) {
+              app.points = app.points + parseInt(app.countries[i].points);
+            } else {
+              app.points = app.points + 5;
             }
-        });
-      }
+
+
+            $('#points').text("You've got" + " " + points + " " +"points");
+
+          }else{
+            console.log("Incorrect answer.", inputValue);
+          }
+      }); //swal
     }
-  });
 }
