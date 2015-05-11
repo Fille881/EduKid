3718a879f1e2a07ace16649d8330fec73b480643
FunnyFrank <tomfledderus@gmail.com>
2015-05-11 22:48:24 +0200

    Added flag icons to conquered countries

--- js/app2.js
+++ js/app2.js
@@ -180,7 +180,7 @@ function showConqueredCountries(playerCounter){
 			$("#list1").append('<p class="list-group-item nav-header list-group-item-info"><small> Countries conquered </small></p>' );
 	 		
 	 		$.each( app.playerCountries.player1, function(i, country) {
-		 		$("#list1").append('<p class="list-group-item"><span class="icon-' + country.code + '" ><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></span><span class="badge">' + country.points + '</span>'  + ' '  + country.name +' </p>' ); 
+		 		$("#list1").append('<p class="list-group-item"><span class="icon-' + country.code + '" ><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"><span class="path13"></span><span class="path14"></span></span></span></span><span class="badge">' + country.points + '</span>'  + ' '  + country.name +' </p>' ); 
 		
 			});	
 		}
